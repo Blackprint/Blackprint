d68f1865e4ca37974356b82b5aa5556fd96a674b
StefansArya <stefansarya1@gmail.com>
2020-08-21 15:59:14 +0700

    Add port manual input and add use data type as port tooltip

---
+++ src/Blackprint/component/port-input.js
@@ -0,0 +1,14 @@
+Space.component('comp-port-input', {
+	template: 'Blackprint/component/port-input.html'
+}, function(self, root){
+	var whichBind = self.which;
+	self.default = self.obj[whichBind];
+
+	self.v2m$default = self.whenChanged || function(old, now){
+		self.obj[whichBind] = now;
+	}
+
+	self.click = function(ev){
+		ev.stopPropagation();
+	}
+});
