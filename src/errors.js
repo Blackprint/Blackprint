6a9b45e7095a08aab9ab3e54fadcab88dfbd85e5
StefansArya <stefansarya1@gmail.com>
2021-12-03 19:45:07 +0700

    Change internal event name

--- src/errors.js
+++ src/errors.js
@@ -2,19 +2,19 @@ let BlackprintEventFallback = {
 	error(error){
 		BlackprintEventFallback.error.types[error.type](error.data || error);
 	},
-	cable_wrong_pair({ port, cable }){
+	'cable.wrong_pair'({ port, cable }){
 		console.log(`The cable is not suitable (${cable.source}, ${port.source})`);
 	},
-	cable_wrong_type({ cable, iface, source, target }){
+	'cable.wrong_type'({ cable, iface, source, target }){
 		console.log(iface.title+"> Port from '"+source.iface.title + " - " + source.name+"' was not an "+target.type.name);
 	},
-	cable_wrong_type_pair({ cable, target }){
+	'cable.wrong_type_pair'({ cable, target }){
 		console.log(`The cable type is not suitable (${cable.owner.type.name}, ${target.type.name})`);
 	},
-	cable_duplicate_removed({ cable, target }){
+	'cable.duplicate_removed'({ cable, target }){
 		console.log("Duplicated cable removed");
 	},
-	cable_replaced({ cable, target }){
+	'cable.replaced'({ cable, target }){
 		console.log("Cable was replaced because input doesn't support array");
 	}
 };
