02c333d1e9662f9bcde1c9c4e609108f4170c2f9
StefansArya <stefansarya1@gmail.com>
2022-01-13 11:31:46 +0700

    Fix loader path for browser

--- dist/_entry.js
+++ dist/_entry.js
@@ -8,5 +8,14 @@ if(window.sf === void 0)
 if(window.Blackprint === void 0)
 	throw new Error("Blackprint Engine must be loaded first before Blackprint Sketch.");
 
-window.sf.loader.js(['./blackprint.min.js', './blackprint.sf.js'], {ordered: true});
-window.sf.loader.css(['./blackprint.sf.css']);
+!function(){
+	let sf = window.sf;
+
+	let path = sf.$('script[src*="blackprint/sketch"]')[0];
+	if(path === void 0) alert("'blackprint/sketch' URL was not found on any <script> tag, make sure to load Blackprint from the correct CDN link");
+
+	path = path.src.split('/dist')[0];
+
+	sf.loader.js([path+'/dist/blackprint.min.js', path+'/dist/blackprint.sf.js'], {ordered: true});
+	sf.loader.css([path+'/dist/blackprint.sf.css']);
+}();
