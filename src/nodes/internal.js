5dd4ea47d1d09705582ffabfa22c5093b6ded65a
StefansArya <stefansarya1@gmail.com>
2022-04-16 15:04:23 +0700

    Prepare custom function and variable node

--- src/nodes/internal.js
+++ src/nodes/internal.js
@@ -23,6 +23,7 @@ let BPFn = {template: 'Blackprint/nodes/default.sf'};
 $(function(){
 	Blackprint.Sketch.registerInterface('BPIC/BP/Var/Get', BPVar, Blackprint._iface['BPIC/BP/Var/Get']);
 	Blackprint.Sketch.registerInterface('BPIC/BP/Var/Set', BPVar, Blackprint._iface['BPIC/BP/Var/Set']);
+	Blackprint.Sketch.registerInterface('BPIC/BP/Fn/Main', BPFn, Blackprint._iface['BPIC/BP/Fn/Main']);
 	Blackprint.Sketch.registerInterface('BPIC/BP/Fn/Input', BPFn, Blackprint._iface['BPIC/BP/Fn/Input']);
 	Blackprint.Sketch.registerInterface('BPIC/BP/Fn/Output', BPFn, Blackprint._iface['BPIC/BP/Fn/Output']);
 });
