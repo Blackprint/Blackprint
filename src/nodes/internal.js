19b6f731e3c0f8479d1e7568173039147ea82e46
StefansArya <stefansarya1@gmail.com>
2022-11-21 13:56:12 +0700

    Put namespace in skeleton node's description and improve style

--- src/nodes/internal.js
+++ src/nodes/internal.js
@@ -8,8 +8,10 @@ Blackprint.registerNode("BP/Skeleton", class extends Blackprint.Node {
 	}
 
 	imported(data){
-		this.iface.title = data.namespace.split('/').slice(-2).join(' ');
+		let temp = data.namespace.split('/');
+		this.iface.title = temp.slice(-2).join(' ');
 		this.iface.namespace = data.namespace;
+		this.iface.description = temp.join(' / ');
 		this.iface.type = 'bp-skeleton';
 	}
 });
